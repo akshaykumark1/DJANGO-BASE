6047b1d7bfa682322eda3587c004fd316f9abfad
akshaykumark1 <kumarakshay90574@gmail.com>
2025-01-09 11:45:04 +0530

    done

--- helo/mypro/myapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-# Create your models here.
-class Gallery(models.Model):
-    classimages=models.ImageField(upload_to='images/')
-    User = models.ForeignKey(User, on_delete=models.CASCADE)
-    
--- new_d/gallery_vault/gallery/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Gallery(models.Model):
-    image = models.ImageField(upload_to='gallery_images/')
-    title = models.CharField(max_length=100)
-    description = models.TextField(max_length=200)
-
-   
